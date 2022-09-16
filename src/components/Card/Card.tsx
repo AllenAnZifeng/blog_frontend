26db4cd5cf1bf0a8854548f8b112526fe1c6c80c
Allen <zifeng.allen@gmail.com>
2022-09-16 15:51:21 -0400

    update

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -22,12 +22,12 @@ export function Card(props: Props) {
         const fetchData = async () => {
             const URL = "https://raw.githubusercontent.com/AllenAnZifeng/blog_content/master/contents/" + props.filename
             let result:string = await fetch(URL).then(res => res.text())
-            let splitted:string[] = result.split("\n",5)
-            let title:string = splitted[0].slice(2)
-            let time:string = splitted[1].split(":")[1].trim()
-            let description:string = splitted[2].split(":")[1].trim()
-            let category:string = splitted[3].split(":")[1].trim()
-            let tags:string[] = splitted[4].split(":")[1].split(',')
+            let splitted:string[] = result.split("\n",6)
+            let title:string = splitted[0].trim()
+            let time:string = splitted[2].split(":")[1].slice(0,-1).trim()
+            let description:string = splitted[3].split(":")[1].slice(0,-1).trim()
+            let category:string = splitted[4].split(":")[1].slice(0,-1).trim()
+            let tags:string[] = splitted[5].split(":")[1].split(',')
             setInfo({
                 title:title,
                 time: time,
