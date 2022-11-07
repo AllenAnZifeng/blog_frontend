423b6967b62d0e59b7f2d5d4c8765bf61b17afd2
Allen <zifeng.allen@gmail.com>
2022-11-06 22:59:31 -0500

    testing for articles url refresh

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -2,22 +2,35 @@ import React, {useEffect, useState} from 'react'
 import './Article.scss'
 import ReactMarkdown from 'react-markdown'
 import { useParams } from "react-router-dom";
+import {useAppSelector} from "../../app/hooks";
+import {selectAllArticles} from "../../features/articles/articleSlice";
 
+
+// export function Article() {
+//     const [data, setData] = useState("");
+//     let filename = useParams().filename;
+//
+//     useEffect( () => {
+//         const fetchData = async () => {
+//             const URL = "https://raw.githubusercontent.com/AllenAnZifeng/blog_content/master/contents/" + filename
+//             let result:string = await fetch(URL).then(res => res.text())
+//             setData(result);
+//         }
+//         fetchData().catch(console.error)
+//     },[filename]);
+//
+//     return <div className={'article'}>
+//         <ReactMarkdown>{data}</ReactMarkdown>
+//     </div>
+// }
 
 export function Article() {
-    const [data, setData] = useState("");
+
+    const articles = useAppSelector(selectAllArticles)
     let filename = useParams().filename;
-
-    useEffect( () => {
-        const fetchData = async () => {
-            const URL = "https://raw.githubusercontent.com/AllenAnZifeng/blog_content/master/contents/" + filename
-            let result:string = await fetch(URL).then(res => res.text())
-            setData(result);
-        }
-        fetchData().catch(console.error)
-    },[filename]);
-
+    let data = articles.filter((article) => article.filename ===filename)
+    console.log(data)
     return <div className={'article'}>
-        <ReactMarkdown>{data}</ReactMarkdown>
+           <ReactMarkdown>{data[0].data}</ReactMarkdown>
     </div>
 }
